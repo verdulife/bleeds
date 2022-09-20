d7328563417b7d604d19d028762629c2c6a30d79
verdulife <verdu@live.com>
2022-09-20 20:09:41 +0200

    multiple images and autorotate images to fit better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,22 @@
 <script>
-	import { PDFDocument, PageSizes } from 'pdf-lib';
+	import { PDFDocument, PageSizes, degrees } from 'pdf-lib';
 	import { onMount } from 'svelte';
 
-	let src, doc, page;
+	let src, doc;
 	let selectedSize = PageSizes.A6;
+	let statusmessage = 'Nothing to process';
+
+	function updateStatus(message) {
+		statusmessage = message;
+	}
 
 	async function createPdf() {
 		doc = await PDFDocument.create();
-		page = doc.addPage(selectedSize);
+	}
 
+	onMount(createPdf);
+
+	async function processPdf(doc) {
 		const pdfDataUri = await doc.saveAsBase64({ dataUri: true });
 		const res = await fetch(pdfDataUri);
 		const blob = await res.blob();
@@ -16,11 +24,9 @@
 		src = url + '#view=fit';
 	}
 
-	onMount(createPdf);
-
 	function readFile(file) {
 		return new Promise((resolve, reject) => {
-			let reader = new FileReader();
+			const reader = new FileReader();
 
 			reader.onload = () => {
 				resolve(reader.result);
@@ -35,39 +41,48 @@
 	function loadFile() {
 		const input = document.createElement('input');
 		input.type = 'file';
+		input.multiple = true;
 		input.accept = 'image/jpeg,image/png,application/pdf';
 		input.click();
 
 		input.onchange = async () => {
-			const rawFile = input.files[0];
-			const { type } = rawFile;
+			const rawFiles = Array.from(input.files);
 
-			if (type === 'application/pdf') console.log('Is PDF');
-			else console.log('Is image');
+			for (let i = 0; i < rawFiles.length; i++) {
+				updateStatus(`Processing image ${i + 1} of ${rawFiles.length}`);
 
-			try {
+				const rawFile = rawFiles[i];
+				const newPage = doc.addPage(selectedSize);
+				const { type } = rawFile;
+
+				if (type === 'application/pdf') console.log('Is PDF');
+				else console.log('Is image');
+
 				const fileBuffer = await readFile(rawFile);
 				const embedImage = await doc.embedJpg(fileBuffer);
-				const imgWidthScale = selectedSize[0] / embedImage.width;
-				const imgHeightScale = selectedSize[1] / embedImage.height;
+				const isHorizontal = embedImage.width > embedImage.height;
+				const printWidth = isHorizontal ? embedImage.height : embedImage.width;
+				const printHeight = isHorizontal ? embedImage.width : embedImage.height;
+				const imgWidthScale = selectedSize[0] / printWidth;
+				const imgHeightScale = selectedSize[1] / printHeight;
 				const imgScale = Math.max(imgWidthScale, imgHeightScale);
 				const imgSize = embedImage.scale(imgScale);
 
-				page.drawImage(embedImage, {
-					x: page.getWidth() / 2 - imgSize.width / 2,
-					y: page.getHeight() / 2 - imgSize.height / 2,
+				if (isHorizontal) {
+					newPage.setSize(selectedSize[1], selectedSize[0]);
+					newPage.setRotation(degrees(90));
+				}
+
+				newPage.drawImage(embedImage, {
+					x: newPage.getWidth() / 2 - imgSize.width / 2,
+					y: newPage.getHeight() / 2 - imgSize.height / 2,
 					width: imgSize.width,
 					height: imgSize.height
 				});
-
-				const pdfDataUri = await doc.saveAsBase64({ dataUri: true });
-				const res = await fetch(pdfDataUri);
-				const blob = await res.blob();
-				const url = URL.createObjectURL(blob);
-				src = url + '#view=fit';
-			} catch (error) {
-				console.log(error);
 			}
+
+			processPdf(doc);
+			updateStatus('Nothing to process');
 		};
 	}
 
@@ -85,9 +100,13 @@
 		</select>
 	</aside>
 
-	{#if src}
-		<iframe class="grow yfill" {src} title="=PDF preview" />
-	{/if}
+	<article class="col grow yfill">
+		<div class="status xfill tcenter">{statusmessage}</div>
+
+		{#if src}
+			<iframe class="fill" {src} title="=PDF preview" />
+		{/if}
+	</article>
 </section>
 
 <style lang="scss">
@@ -98,5 +117,15 @@
 
 	iframe {
 		border: none;
+	}
+
+	article {
+		background: #000;
+	}
+
+	.status {
+		background: var(--color-sec);
+		color: #fff;
+		padding: 5px;
 	}
 </style>
